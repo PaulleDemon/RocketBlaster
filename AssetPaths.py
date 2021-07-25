7918b0b8908e2a51854e5367b13cf981bf12d820
Paul <paulsajo0@gmail.com>
2021-07-25 08:00:49 +0530

    added sound effects and few game changes

--- AssetPaths.py
+++ AssetPaths.py
@@ -1,4 +1,8 @@
-
+# images
 ROCKET = r"Assets/spaceship.png"
 MISSILE = r"Assets/missile.png"
 SPACE_MONSTER = r"Assets/spacemonster.png"
+
+# sounds
+GUNSHOT = r"Assets/Gunshot.mp3"
+MONSTER_DEATH = r"Assets/monster_death.wav"
--- main.py
+++ main.py
@@ -13,6 +13,8 @@ screen = pygame.display.set_mode(size)
 pygame.display.set_caption("RocketBlaster")
 
 running = True
+game_over = False
+
 clock = pygame.time.Clock()
 
 rocket_image = pygame.image.load(AssetPaths.ROCKET)
@@ -30,13 +32,24 @@ bullet_speed = 5
 max_bullets = 5
 missile_angle = rocket_angle
 
-counter = 50
+counter = 60
 monster_spawn_counter = counter  # spans after 30 seconds
 max_monsters = max_bullets - 1
 monster_speed = 1
 
 bullet_lst = []
 monster_lst = []
+
+score = 0
+score_font = pygame.font.SysFont('Consolas', 30)
+game_over_font = pygame.font.SysFont('Times', 50, True)
+
+pygame.mixer.init()
+shoot_sound = pygame.mixer.Sound(AssetPaths.GUNSHOT)
+shoot_sound.set_volume(0.5)
+
+death_sound = pygame.mixer.Sound(AssetPaths.MONSTER_DEATH)
+death_sound.set_volume(0.5)
 
 
 class Missile:
@@ -56,7 +69,12 @@ class SpaceMonster:
 
 def create_monsters():
     global monster_spawn_counter
+
     monster_spawn_counter -= 1
+
+    if monster_spawn_counter < 0:
+        monster_spawn_counter = 0
+
     if len(monster_lst) < max_monsters and monster_spawn_counter == 0:
         rand_x = random.randint(50, screen.get_width() - 100)
         rand_y = random.randint(50, 150)
@@ -67,31 +85,41 @@ def create_monsters():
 
 
 def detect_collisions():
+    global score
+
     for monster in monster_lst.copy():
+
+        monster_rect = monster_image.get_rect()
+
+        monster_rect[0] += monster.pos[0]
+        monster_rect[1] += monster.pos[1]
+        monster_rect[2] += monster.pos[0]
+        monster_rect[3] += monster.pos[1]
+
         for bullet in bullet_lst.copy():
-
-            monster_rect = monster_image.get_rect()
-
-            monster_rect[0] += monster.pos[0]
-            monster_rect[1] += monster.pos[1]
-            monster_rect[2] += monster.pos[0]
-            monster_rect[3] += monster.pos[1]
 
             if monster_rect[0] < bullet.bullet_pos[0] < monster_rect[2] and \
                     (monster_rect[1] < bullet.bullet_pos[1] < monster_rect[3]):
 
-                monster_lst.remove(monster)
-                bullet_lst.remove(bullet)
+                try:
+                    monster_lst.remove(monster)
+                    bullet_lst.remove(bullet)
+                    death_sound.play()
+
+                except ValueError:
+                    pass
+
+                score += 1
 
 
 def move_monsters():
-    global running
+    global game_over
     for monster in monster_lst:
         monster.pos = monster.pos[0], monster.pos[1] + monster_speed
         screen.blit(monster_image, monster.pos)
 
         if monster.pos[1] > screen.get_height() - 100:
-            running = False
+            game_over = True
 
 
 def move_bullet():
@@ -125,10 +153,28 @@ def rocket(mousepos: tuple[float, float]):
 
     adj_dist, opp_dist, hyp = get_adj_opp(mousepos)
 
-    rocket_angle = math.degrees(math.atan2(opp_dist, -adj_dist)) + 90
+    rocket_angle = math.degrees(math.atan2(opp_dist/hyp, -adj_dist/hyp)) + 90
     rotated = pygame.transform.rotate(rocket_image, rocket_angle)
 
     screen.blit(rotated, rocket_pos)
+
+
+def change_game_speed():
+    global monster_speed, counter, max_monsters, max_bullets
+
+    if score > 0:
+        if score % 50 == 0:
+            monster_speed += 0.001
+
+        if score % 100 == 0 and counter > 20:
+            counter -= 1
+
+        if counter == 20:
+            counter = 50
+
+        if score % 125 == 0:
+            max_monsters += 1
+            max_bullets += 1
 
 
 while running:
@@ -136,6 +182,7 @@ while running:
     screen.fill(color=Colors.BLACK)
 
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
             running = False
 
@@ -150,11 +197,30 @@ while running:
                 bullet = Missile(-rocket_angle, bullet_x, bullet_y)
                 bullet_lst.append(bullet)
 
+                shoot_sound.play()
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and game_over:
+                game_over = False
+                score = 0
+                monster_lst = []
+                bullet_lst = []
+
     rocket(pygame.mouse.get_pos())
-    move_bullet()
-    create_monsters()
-    move_monsters()
-    detect_collisions()
+
+    if not game_over:
+        move_bullet()
+        create_monsters()
+        move_monsters()
+        detect_collisions()
+
+        change_game_speed()
+
+    else:
+        screen.blit(game_over_font.render("Game over.", True, (255, 255, 255)), (100, screen.get_height() / 3))
+        screen.blit(game_over_font.render("Press Space to play again", True, (255, 255, 255)), (100, screen.get_height() / 2))
+
+    screen.blit(score_font.render(f"Score: {score}", True, (255, 255, 255)), (screen.get_width()-200, 50))
 
     for bullet in bullet_lst.copy():
         if 0 < bullet.bullet_pos[0] > screen.get_width() or bullet.bullet_pos[1] < 0:
